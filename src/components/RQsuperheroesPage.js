25052fafa78f5152f0132a403eff081d32694c50
shirin-nazari <74411429+shirin-nazari@users.noreply.github.com>
2022-08-26 23:46:58 +0430

    use query

--- src/components/RQsuperheroesPage.js
+++ src/components/RQsuperheroesPage.js
@@ -1,8 +1,11 @@
+import axios from 'axios'
 import React from 'react'
 import { useQuery } from 'react-query'
 
 function RQsuperheroesPage() {
-    useQuery()
+    const resualts = useQuery('super-heroes', () => {
+        return axios.get('http://localhost:4000/superheroes')
+    })
     return (
         <div>RQsuperheroesPage</div>
     )
