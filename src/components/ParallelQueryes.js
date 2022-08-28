4f01fdc3137fda8a9e3049e466112d111df971c3
shirin-nazari <74411429+shirin-nazari@users.noreply.github.com>
2022-08-29 00:07:44 +0430

    add

--- src/components/ParallelQueryes.js
+++ src/components/ParallelQueryes.js
@@ -1,6 +1,11 @@
+import axios from 'axios'
 import React from 'react'
-
+import { useQuery } from 'react-query'
+const fetchSuperHeroes = () => axios.get('http://localhost:4000/superheroes')
+const fetchFriends = () => axios.get('http://localhost:4000/friends')
 function ParallelQueryes() {
+    const { data: superHeroes } = useQuery('super-heroes', fetchSuperHeroes)
+    const { data: friends } = useQuery('friends', fetchFriends)
     return (
         <div>ParallelQueryes</div>
     )
