4126de3ea68a1bb3c4ccc2e10635aa0596e721c4
shirin-nazari <74411429+shirin-nazari@users.noreply.github.com>
2022-09-01 12:36:24 +0430

    add file

--- src/components/DynamicParallelPage.js
+++ src/components/DynamicParallelPage.js
@@ -1,7 +1,17 @@
 import axios from "axios"
+import { useQueries } from "react-query"
 
 const fetchSuperHeroes = (heroId) => axios.get(`http://localhost:4000/superheroes/${heroId}`)
 const DynamicParallelPage = ({ heroId }) => {
+    const queryResults = useQueries(
+        heroId.map(id => {
+            return {
+                queryKey: ['super-hero', id],
+                queryFn: () => fetchSuperHeroes(id)
+            }
+        })
+    )
+    console.log({ queryResults });
     return (
         <div>Dyanamic</div>
     )
